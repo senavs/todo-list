8e354be463088c0a16b59d0f72d5a87a7c1706f5
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-02-01 21:11:32 -0300

    release: change version to 1.0.0

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -4,7 +4,7 @@ from .database.core import Bootloader
 from .modules.errorhandler import ErrorHandler
 from .routes import router
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 app = FastAPI(title='TO DO list API',
               description='To do list WEB application with Python, NodeJS and React.',
